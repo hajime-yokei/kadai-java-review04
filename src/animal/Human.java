35951c3f9f2c16c66f9acac18058ac069927dfcd
hajime-yokei <hyokei614@gmail.com>
2023-02-08 21:12:45 +0900

    コミット

---
+++ src/animal/Human.java
@@ -0,0 +1,25 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+    private String hobby;
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Human() {
+    }
+
+    public Human(String name, int age, String hobby) {
+        super(name, age);
+        this.hobby = hobby;
+    }
+
+    public void think() {
+        System.out.println("私は" + hobby + "について考えています。");
+    }
+}
